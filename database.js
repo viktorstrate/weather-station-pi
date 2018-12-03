ecf821660e163ab1c9cf41e13035293e2fa27b02
viktorstrate <viktorstrate@gmail.com>
2018-12-03 10:31:08 +0100

    Fix promise

--- database.js
+++ database.js
@@ -3,9 +3,9 @@ const fs = require('fs')
 
 const db_path = './database.sqlite'
 
-function setup_db(db) {
+function setup_db(db, resolve) {
   const query = fs.readFileSync('./setup.sql', 'utf8')
-  db.run(query)
+  db.run(query, () => resolve(db))
   console.log('database tables made successfully')
 }
 
@@ -18,7 +18,7 @@ module.exports = new Promise((resolve, reject) => {
 
     if (fs.statSync(db_path).size == 0) {
       console.log('db not initialized, making tables')
-      setup_db(db)
+      setup_db(db, resolve)
     } else {
       resolve(db)
     }
