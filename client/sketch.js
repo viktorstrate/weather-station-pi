0fb9a70eec0e8924495ce22c102bd77114d2f2a8
Gibbus2 <gibbusf@gmail.com>
2018-12-04 10:22:17 +0100

    refactor sketch and remove positioning of canvas

--- client/sketch.js
+++ client/sketch.js
@@ -24,8 +24,6 @@ var xAjust, yAjust
 function setup() {
   can = createCanvas(800, 400)
 
-  positioning()
-
   frameRate(1)
 }
 
@@ -48,40 +46,30 @@ function graphAjustment(arr){
   var xmax = -1
   var xmin = Infinity
   var xAjust
-  
-  for(var i = 0; i < arr.length; i++){
-    if(arr[i][0] > xmax){
-      xmax = arr[i][0]
-    }
-    if(dataArr[i][0] < xmin){
-      xmin = arr[i][0]
-    }
-  }
-  xAjust = width / (xmax - xmin)
-
   var ymax = -1
   var ymin = Infinity
   var yAjust
 
-  for(var i = 0; i < arr.length; i++){
-    if(arr[i][1] > ymax){
-      ymax = arr[i][1]
+  for(var item of arr){
+    if(item[0] > xmax){
+      xmax = item[0]
     }
-    if(arr[i][1] < ymin){
-      ymin = arr[i][1]
+    if(item[0] < xmin){
+      xmin = item[0]
+    }
+    
+    if(item[1] > ymax){
+      ymax = item[1]
+    }
+    if(item[1] < ymin){
+      ymin = item[1]
     }
   }
+  xAjust = width / (xmax - xmin)
   yAjust = height / (ymax - ymin + yMargin*2)
 
   for(var item of arr){
     returnArray.push( [((item[0] - xmin) * xAjust) * (-1) + width, ((item[1] - ymin + yMargin) * yAjust) * (-1) + height] )
   }
   return returnArray
-}
-
-function positioning(){
-  //position canvas
-  var canx = (windowWidth - width) / 2
-  var cany = (windowHeight - height) / 2
-  can.position(canx, cany)
 }
