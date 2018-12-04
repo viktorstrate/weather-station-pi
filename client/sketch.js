dd8d320dc4609fc5ef1f8ada2b5de5202c1102f4
Gibbus2 <gibbusf@gmail.com>
2018-12-04 10:12:42 +0100

    refactor sketch

--- client/sketch.js
+++ client/sketch.js
@@ -12,61 +12,68 @@ socket.on('light_sensor_start_values', data => {
   dataArr = data
 })
 
-var xmin, ymin
-var xAjust, yAjust
-
 //runs once at program start
 function setup() {
   can = createCanvas(800, 400)
 
-  //position canvas
-  var canx = (windowWidth - width) / 2
-  var cany = (windowHeight - height) / 2
-  can.position(canx, cany)
+  positioning()
+
+  frameRate(1)
 }
 
 //Infinite loop
 function draw() {
   background(100)
 
-
-  print(dataArr)
-  graphAjustment() 
-
   noFill()
+  var array = graphAjustment(dataArr)
   beginShape()
-  for(var i = 0; i < dataArr.length; i++){
-    vertex(((dataArr[i][0] - xmin) * xAjust) * (-1) + width, ((dataArr[i][1] - ymin + yMargin) * yAjust) * (-1) + height)
+  for(var i = 0; i < array.length; i++){
+    vertex(array[i][0], array[i][1])
   }
   endShape()
-  noLoop()
 }
 
 
-function graphAjustment(){
-  //ajust x-axis
+function graphAjustment(arr){
+  var returnArray = new Array()
   var xmax = -1
-  xmin = Infinity
-  for(var i = 0; i < dataArr.length; i++){
-    if(dataArr[i][0] > xmax){
-      xmax = dataArr[i][0]
+  var xmin = Infinity
+  var xAjust
+  
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i][0] > xmax){
+      xmax = arr[i][0]
     }
     if(dataArr[i][0] < xmin){
-      xmin = dataArr[i][0]
+      xmin = arr[i][0]
     }
   }
   xAjust = width / (xmax - xmin)
 
-  //ajust y-axis
   var ymax = -1
-  ymin = Infinity
-  for(var i = 0; i < dataArr.length; i++){
-    if(dataArr[i][1] > ymax){
-      ymax = dataArr[i][1]
+  var ymin = Infinity
+  var yAjust
+
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i][1] > ymax){
+      ymax = arr[i][1]
     }
-    if(dataArr[i][1] < ymin){
-      ymin = dataArr[i][1]
+    if(arr[i][1] < ymin){
+      ymin = arr[i][1]
     }
   }
   yAjust = height / (ymax - ymin + yMargin*2)
+
+  for(var item of arr){
+    returnArray.push( [((item[0] - xmin) * xAjust) * (-1) + width, ((item[1] - ymin + yMargin) * yAjust) * (-1) + height] )
+  }
+  return returnArray
+}
+
+function positioning(){
+  //position canvas
+  var canx = (windowWidth - width) / 2
+  var cany = (windowHeight - height) / 2
+  can.position(canx, cany)
 }
